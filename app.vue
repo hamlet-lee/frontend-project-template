f742ba2e684dfaff37f5d15564752c15c71a1eaa
Sining Li <lisn@rd.netease.com>
2018-01-14 20:25:24 +0800

    ADD: initial version

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Hello {{ name }} </div>
+</template>
+<script>
+  export default {
+    data(){
+      return {
+       name: 'Vue.js'
+      }
+    }
+  }
+</script>
+<style scoped>
+  div{
+    color: red;
+    fontsize: 24px;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const config = {
+  entry: {
+    main: './main',
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: '/dist',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+       {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              css: ExtractTextPlugin.extract({
+                 use: 'css-loader',
+                 fallback: 'vue-style-loader'
+              })
+            }
+          }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+         test: /\.css$/,
+         use: ExtractTextPlugin.extract({
+             use: 'css-loader',
+             fallback: 'style-loader'
+           })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css')
+  ]
+};
+module.exports = config;
