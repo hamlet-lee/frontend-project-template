b94126b16c5160f720aa22774b7d39b94ff9ad69
Sining Li <lisn@rd.netease.com>
2018-01-26 22:10:32 +0800

    ADD: scroll top; ADD: vue-router's path param; ADD: vue-router title

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 import Vue from 'vue';
 import App from './app.vue';
 import VueRouter from 'vue-router';
+import './style.css';
 
 Vue.use(VueRouter);
 
 const Routes = [
 	{
 		path: '/index',
-		component: (resolve) => require(['./views/index.vue'], resolve)
+		component: (resolve) => require(['./views/index.vue'], resolve),
+		meta: {
+			title: "首页"
+		}
 	},
 	{
 		path: '/about',
-		component: (resolve) => require(['./views/about.vue'], resolve)
+		component: (resolve) => require(['./views/about.vue'], resolve),
+		meta: {
+			title: "关于"
+		}
 	},
-	/*{
+	{
 		path: '/user/:id',
-		component: (resolve) => require(['./views/user'], resolve)
-	},*/
+		component: (resolve) => require(['./views/user.vue'], resolve),
+		meta: {
+			title: "用户"
+		}
+	},
 	{
 		path: '*',
 		redirect: '/index'
@@ -29,7 +39,13 @@ const RouterConfig = {
 };
 
 const router = new VueRouter(RouterConfig);
-
+router.beforeEach( (to,from,next) => {
+	window.document.title = to.meta.title;
+	next();
+});
+router.afterEach( (to,from,next) => {
+	window.scrollTo(0,0);
+});
 new Vue({
   el: "#app",
   router: router,
--- views/index.vue
+++ views/index.vue
@@ -1,7 +1,25 @@
  <template>
-	<div>首页</div>
+	<div>
+		<div>首页</div>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<router-link to="/about">跳转到 about</router-link>
+		<router-link :to=" getTarget() " replace>跳转到user</router-link>
+		<button @click="handleClick">跳转到user</button>
+		<button @click="handleClickReplace">跳转到user (replace)</button>
+	</div>
  </template>
  <script>
 	export default {
+		methods: {
+			getTarget: function(){
+				return "/user/135";
+			},
+			handleClick: function(){
+				this.$router.push('/user/453');
+			},
+			handleClickReplace: function(){
+				this.$router.replace('/user/453');
+			}
+		}
 	}
  </script>
